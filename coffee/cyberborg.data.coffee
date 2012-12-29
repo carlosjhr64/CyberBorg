9e3eb1f9d3678f6cedeabae6e810e0400b4cb347
carrot <carlosjhr64@gmail.com>
2012-12-29 11:09:21 -0800

    CyberBorg::factory_orders

--- coffee/cyberborg.data.coffee
+++ coffee/cyberborg.data.coffee
@@ -1,3 +1,9 @@
+Number::times = (action) ->
+  i = 0
+  while i < this.valueOf()
+    action()
+    i++
+
 CyberBorg::base_orders = ->
   [
     
@@ -90,88 +96,18 @@ CyberBorg::base_orders = ->
   ]
 
 CyberBorg::factory_orders = ->
-  [
-    name: "Truck"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_CONSTRUCT
-    turret: "Spade1Mk1"
-  ,
-    name: "Truck"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_CONSTRUCT
-    turret: "Spade1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ,
-    name: "MgWhB1"
-    body: "Body1REC"
-    propulsion: "wheeled01"
-    droid_type: DROID_WEAPON
-    turret: "MG1Mk1"
-  ]
+  # A wheeled viper
+  whb1 = (droid) ->
+    droid.body = "Body1REC"; droid.propulsion = "wheeled01"
+    droid
+  # A wheeled truck
+  truck = name: "Truck", turret: "Spade1Mk1", droid_type: DROID_CONSTRUCT
+  # A wheeled machine gunner
+  mg1 = name: "MgWhB1", turret: "MG1Mk1", droid_type: DROID_WEAPON
+  # The orders are...
+  orders = []
+  # ... 2 trucks
+  (2).times -> orders.push(whb1(truck))
+  # ... 12 machine gunners
+  (12).times -> orders.push(whb1(mg1))
+  orders
--- js/cyberborg.data.js
+++ js/cyberborg.data.js
@@ -1,36 +1,140 @@
-CyberBorg.prototype.base_orders=function(){
-  return( [
-  // Phase 1  Build up the initial base as fast a posible
-  {min:3, max:3, structure:"A0LightFactory",     at:{x:9, y:234}},
-  {min:3, max:3, structure:"A0ResearchFacility", at:{x:6, y:234}},
-  {min:3, max:3, structure:"A0CommandCentre",    at:{x:6, y:237}},
-  {min:3, max:3, structure:"A0PowerGenerator",   at:{x:3, y:234}},
-  // Phase 2  Just have one truck max out the base with research and power
-  {min:1, max:1, structure:"A0ResearchFacility", at:{x:3, y:237}},
-  {min:1, max:1, structure:"A0PowerGenerator",   at:{x:3, y:240}},
-  {min:1, max:1, structure:"A0ResearchFacility", at:{x:6, y:240}},
-  {min:1, max:1, structure:"A0PowerGenerator",   at:{x:9, y:240}},
-  {min:1, max:1, structure:"A0ResearchFacility", at:{x:12,y:240}},
-  {min:1, max:1, structure:"A0PowerGenerator",   at:{x:12,y:243}},
-  {min:1, max:1, structure:"A0ResearchFacility", at:{x:9, y:243}},
-  {min:1, max:1, structure:"A0PowerGenerator",   at:{x:6, y:243}},
-  ]);
-}
-CyberBorg.prototype.factory_orders=function(){
-  return([
-  {name:"Truck", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_CONSTRUCT, turret:"Spade1Mk1",},
-  {name:"Truck", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_CONSTRUCT, turret:"Spade1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  {name:"MgWhB1", body:"Body1REC", propulsion:"wheeled01", droid_type:DROID_WEAPON, turret:"MG1Mk1",},
-  ]);
-}
+
+Number.prototype.times = function(action) {
+  var i, _results;
+  i = 0;
+  _results = [];
+  while (i < this.valueOf()) {
+    action();
+    _results.push(i++);
+  }
+  return _results;
+};
+
+CyberBorg.prototype.base_orders = function() {
+  return [
+    {
+      min: 3,
+      max: 3,
+      structure: "A0LightFactory",
+      at: {
+        x: 9,
+        y: 234
+      }
+    }, {
+      min: 3,
+      max: 3,
+      structure: "A0ResearchFacility",
+      at: {
+        x: 6,
+        y: 234
+      }
+    }, {
+      min: 3,
+      max: 3,
+      structure: "A0CommandCentre",
+      at: {
+        x: 6,
+        y: 237
+      }
+    }, {
+      min: 3,
+      max: 3,
+      structure: "A0PowerGenerator",
+      at: {
+        x: 3,
+        y: 234
+      }
+    }, {
+      min: 1,
+      max: 1,
+      structure: "A0ResearchFacility",
+      at: {
+        x: 3,
+        y: 237
+      }
+    }, {
+      min: 1,
+      max: 1,
+      structure: "A0PowerGenerator",
+      at: {
+        x: 3,
+        y: 240
+      }
+    }, {
+      min: 1,
+      max: 1,
+      structure: "A0ResearchFacility",
+      at: {
+        x: 6,
+        y: 240
+      }
+    }, {
+      min: 1,
+      max: 1,
+      structure: "A0PowerGenerator",
+      at: {
+        x: 9,
+        y: 240
+      }
+    }, {
+      min: 1,
+      max: 1,
+      structure: "A0ResearchFacility",
+      at: {
+        x: 12,
+        y: 240
+      }
+    }, {
+      min: 1,
+      max: 1,
+      structure: "A0PowerGenerator",
+      at: {
+        x: 12,
+        y: 243
+      }
+    }, {
+      min: 1,
+      max: 1,
+      structure: "A0ResearchFacility",
+      at: {
+        x: 9,
+        y: 243
+      }
+    }, {
+      min: 1,
+      max: 1,
+      structure: "A0PowerGenerator",
+      at: {
+        x: 6,
+        y: 243
+      }
+    }
+  ];
+};
+
+CyberBorg.prototype.factory_orders = function() {
+  var mg1, orders, truck, whb1;
+  whb1 = function(droid) {
+    droid.body = "Body1REC";
+    droid.propulsion = "wheeled01";
+    return droid;
+  };
+  truck = {
+    name: "Truck",
+    turret: "Spade1Mk1",
+    droid_type: DROID_CONSTRUCT
+  };
+  mg1 = {
+    name: "MgWhB1",
+    turret: "MG1Mk1",
+    droid_type: DROID_WEAPON
+  };
+  orders = [];
+  2..times(function() {
+    return orders.push(whb1(truck));
+  });
+  12..times(function() {
+    return orders.push(whb1(mg1));
+  });
+  return orders;
+};
