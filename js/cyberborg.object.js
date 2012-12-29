41a669db60a73a5179aabd9d1baaabe16e7650d7
carrot <carlosjhr64@gmail.com>
2012-12-29 12:32:05 -0800

    JS Object extensions

--- js/cyberborg.object.js
+++ js/cyberborg.object.js
@@ -1,15 +1,19 @@
-// Object extensions for Droids
-// TODO use map to convert only those objects that need this?
-Object.prototype.build=function(structure_id, pos, direction){
-  return( orderDroidBuild(this, DORDER_BUILD, structure_id, pos.x, pos.y, direction) );
-}
 
-Object.prototype.namexy=function(){
-  return(this.name+'('+this.x+','+this.y+')');
-}
+Object.prototype.build = function(structure_id, pos, direction) {
+  return orderDroidBuild(this, DORDER_BUILD, structure_id, pos.x, pos.y, direction);
+};
 
-Object.prototype.position=function(){
-  return({x:this.x,y:this.y});
-}
+Object.prototype.namexy = function() {
+  return this.name + "(" + this.x + "," + this.y + ")";
+};
 
-Object.prototype.is_truck=function(){ return(CyberBorg.is_truck(this)); }
+Object.prototype.position = function() {
+  return {
+    x: this.x,
+    y: this.y
+  };
+};
+
+Object.prototype.is_truck = function() {
+  return CyberBorg.is_truck(this);
+};
