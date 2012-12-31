0514dfc87cb94e31f0c31efdfbc0c13d758c7099
carrot <carlosjhr64@gmail.com>
2012-12-31 10:14:49 -0800

    Starting to mv class into their own files.

--- js/player_assist.js
+++ js/player_assist.js
@@ -1,6 +1,26 @@
 var CyberBorg, Group, WZArray, WZObject, base_group, cyberBorg, eventChat, eventDroidBuilt, eventStartLevel, eventStructureBuilt, factory_group, getObjectClass, min_map_and_design_on, report,
   __slice = Array.prototype.slice;
 
+Number.prototype.times = function(action) {
+  var i, _results;
+  i = 0;
+  _results = [];
+  while (i < this.valueOf()) {
+    action();
+    _results.push(i++);
+  }
+  return _results;
+};
+
+getObjectClass = function(obj) {
+  var arr;
+  if (obj && obj.constructor && obj.constructor.toString) {
+    arr = obj.constructor.toString().match(/function\s*(\w+)/);
+    if (arr && (arr.length === 2)) return arr[1];
+  }
+  return;
+};
+
 WZObject = (function() {
 
   function WZObject(object) {
@@ -42,6 +62,168 @@ WZObject = (function() {
   };
 
   return WZObject;
+
+})();
+
+WZArray = (function() {
+
+  function WZArray() {}
+
+  WZArray.INIT = -1;
+
+  WZArray.NONE = -1;
+
+  WZArray.bless = function(array) {
+    var method, name, _ref;
+    if (array.is_wzarray) return array;
+    _ref = WZArray.prototype;
+    for (name in _ref) {
+      method = _ref[name];
+      array[name] = method;
+    }
+    array.is_wzarray = true;
+    return array;
+  };
+
+  /* QUERIES
+  */
+
+  WZArray.prototype.indexOfObject = function(object) {
+    var i, id;
+    id = object.id;
+    i = 0;
+    while (i < this.length) {
+      if (this[i].id === id) return i;
+      i++;
+    }
+    return WZArray.NONE;
+  };
+
+  WZArray.prototype.contains = function(object) {
+    return this.indexOfObject(object) > WZArray.NONE;
+  };
+
+  WZArray.prototype.removeObject = function(object) {
+    var i;
+    i = this.indexOfObject(object);
+    if (i > WZArray.NONE) this.splice(i, 1);
+    return i;
+  };
+
+  /* FILTERS
+  */
+
+  WZArray.prototype.filters = function(type) {
+    return WZArray.bless(this.filter(type));
+  };
+
+  WZArray.prototype.trucks = function() {
+    return this.filters(CyberBorg.is_truck);
+  };
+
+  WZArray.prototype.factories = function() {
+    return this.filters(CyberBorg.is_factory);
+  };
+
+  WZArray.prototype.not_built = function() {
+    return this.filters(CyberBorg.not_built);
+  };
+
+  WZArray.prototype.not_in = function(group) {
+    return this.filters(function(object) {
+      return group.group.indexOfObject(object) === WZArray.NONE;
+    });
+  };
+
+  WZArray.prototype["in"] = function(group) {
+    return this.filters(function(object) {
+      return group.group.indexOfObject(object) > WZArray.NONE;
+    });
+  };
+
+  WZArray.prototype.idle = function() {
+    return this.filters(CyberBorg.is_idle);
+  };
+
+  /* SORTS
+  */
+
+  WZArray.prototype.nearest = function(at) {
+    this.sort(function(a, b) {
+      return CyberBorg.nearest_metric(a, b, at);
+    });
+    return this;
+  };
+
+  /* SUMARIES
+  */
+
+  WZArray.prototype.counts = function(type) {
+    var count, i;
+    count = 0;
+    i = 0;
+    while (i < this.length) {
+      if (type(this[i])) count += 1;
+      i++;
+    }
+    return count;
+  };
+
+  WZArray.prototype.center = function() {
+    var at, i, n;
+    at = {
+      x: 0,
+      y: 0
+    };
+    n = this.length;
+    i = 0;
+    while (i < n) {
+      at.x += this[i].x;
+      at.y += this[i].y;
+      i++;
+    }
+    at.x = at.x / n;
+    at.y = at.y / n;
+    return at;
+  };
+
+  /* ACCESSING
+  */
+
+  WZArray.prototype.first = function() {
+    return this[0];
+  };
+
+  WZArray.prototype._current = WZArray.INIT;
+
+  WZArray.prototype.current = WZArray[WZArray._current];
+
+  WZArray.prototype.next = function(gameobj) {
+    var order;
+    if (this._current < this.length) this._current += 1;
+    order = this[this._current];
+    if (gameobj) this.is[gameobj.id] = order;
+    return order;
+  };
+
+  WZArray.prototype.previous = function(gameobj) {
+    var order;
+    if (this._current > WZArray.init) this._current -= 1;
+    order = this[this._current];
+    if (gameobj) this.is[gameobj.id] = order;
+    return order;
+  };
+
+  /* STORES
+  */
+
+  WZArray.prototype.is = {};
+
+  WZArray.prototype.of = function(object) {
+    return this.is[object.id];
+  };
+
+  return WZArray;
 
 })();
 
@@ -242,188 +424,6 @@ Group = (function() {
   };
 
   return Group;
-
-})();
-
-Number.prototype.times = function(action) {
-  var i, _results;
-  i = 0;
-  _results = [];
-  while (i < this.valueOf()) {
-    action();
-    _results.push(i++);
-  }
-  return _results;
-};
-
-getObjectClass = function(obj) {
-  var arr;
-  if (obj && obj.constructor && obj.constructor.toString) {
-    arr = obj.constructor.toString().match(/function\s*(\w+)/);
-    if (arr && (arr.length === 2)) return arr[1];
-  }
-  return;
-};
-
-WZArray = (function() {
-
-  function WZArray() {}
-
-  WZArray.INIT = -1;
-
-  WZArray.NONE = -1;
-
-  WZArray.bless = function(array) {
-    var method, name, _ref;
-    if (array.is_wzarray) return array;
-    _ref = WZArray.prototype;
-    for (name in _ref) {
-      method = _ref[name];
-      array[name] = method;
-    }
-    array.is_wzarray = true;
-    return array;
-  };
-
-  /* QUERIES
-  */
-
-  WZArray.prototype.indexOfObject = function(object) {
-    var i, id;
-    id = object.id;
-    i = 0;
-    while (i < this.length) {
-      if (this[i].id === id) return i;
-      i++;
-    }
-    return WZArray.NONE;
-  };
-
-  WZArray.prototype.contains = function(object) {
-    return this.indexOfObject(object) > WZArray.NONE;
-  };
-
-  WZArray.prototype.removeObject = function(object) {
-    var i;
-    i = this.indexOfObject(object);
-    if (i > WZArray.NONE) this.splice(i, 1);
-    return i;
-  };
-
-  /* FILTERS
-  */
-
-  WZArray.prototype.filters = function(type) {
-    return WZArray.bless(this.filter(type));
-  };
-
-  WZArray.prototype.trucks = function() {
-    return this.filters(CyberBorg.is_truck);
-  };
-
-  WZArray.prototype.factories = function() {
-    return this.filters(CyberBorg.is_factory);
-  };
-
-  WZArray.prototype.not_built = function() {
-    return this.filters(CyberBorg.not_built);
-  };
-
-  WZArray.prototype.not_in = function(group) {
-    return this.filters(function(object) {
-      return group.group.indexOfObject(object) === WZArray.NONE;
-    });
-  };
-
-  WZArray.prototype["in"] = function(group) {
-    return this.filters(function(object) {
-      return group.group.indexOfObject(object) > WZArray.NONE;
-    });
-  };
-
-  WZArray.prototype.idle = function() {
-    return this.filters(CyberBorg.is_idle);
-  };
-
-  /* SORTS
-  */
-
-  WZArray.prototype.nearest = function(at) {
-    this.sort(function(a, b) {
-      return CyberBorg.nearest_metric(a, b, at);
-    });
-    return this;
-  };
-
-  /* SUMARIES
-  */
-
-  WZArray.prototype.counts = function(type) {
-    var count, i;
-    count = 0;
-    i = 0;
-    while (i < this.length) {
-      if (type(this[i])) count += 1;
-      i++;
-    }
-    return count;
-  };
-
-  WZArray.prototype.center = function() {
-    var at, i, n;
-    at = {
-      x: 0,
-      y: 0
-    };
-    n = this.length;
-    i = 0;
-    while (i < n) {
-      at.x += this[i].x;
-      at.y += this[i].y;
-      i++;
-    }
-    at.x = at.x / n;
-    at.y = at.y / n;
-    return at;
-  };
-
-  /* ACCESSING
-  */
-
-  WZArray.prototype.first = function() {
-    return this[0];
-  };
-
-  WZArray.prototype._current = WZArray.INIT;
-
-  WZArray.prototype.current = WZArray[WZArray._current];
-
-  WZArray.prototype.next = function(gameobj) {
-    var order;
-    if (this._current < this.length) this._current += 1;
-    order = this[this._current];
-    if (gameobj) this.is[gameobj.id] = order;
-    return order;
-  };
-
-  WZArray.prototype.previous = function(gameobj) {
-    var order;
-    if (this._current > WZArray.init) this._current -= 1;
-    order = this[this._current];
-    if (gameobj) this.is[gameobj.id] = order;
-    return order;
-  };
-
-  /* STORES
-  */
-
-  WZArray.prototype.is = {};
-
-  WZArray.prototype.of = function(object) {
-    return this.is[object.id];
-  };
-
-  return WZArray;
 
 })();
 
