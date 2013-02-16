913a3a2459582a6c4056ec8b8d28fa9333feffa1
carrot <carlosjhr64@gmail.com>
2013-02-15 17:43:28 -0800

    Can't get consistent results.  :-??

--- cyberborg.js
+++ cyberborg.js
@@ -16831,7 +16831,7 @@ Ai = (function() {
     this.repair_on_damage = 50.0;
     this.repair_available = false;
     this.chances = 96.0;
-    this.power_type_factor = 1.0 / 2.0;
+    this.power_type_factor = 1.0 / 4.0;
     this.too_dangerous = this.too_dangerous_level();
   }
 
